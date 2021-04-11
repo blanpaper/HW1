6944a2015459c29b35bab8dfe22a3d1c71c8a782
blanpaper <web_per@naver.com>
2021-04-11 18:56:58 +0900

    first commit

---
+++ Assets/New Folder/Bullet.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy_Tag")
+        {
+
+            Debug.Log("collision");
+            Destroy(gameObject, 0);
+            Destroy(other.gameObject, 0);
+            // Instantiate(fx_obj, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+        }
+    }
+
+
+}
---
+++ Assets/New Folder/player.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    //private Animator ani;
+
+    private Transform tra;
+    //  private float hor =  0.0f;
+    //   private float ver = 0.0f;
+
+    public float moving_speed = 5.0f;
+    public float rot_speed = 10.0f;
+
+    public GameObject obj;
+
+   private void player_Moving_Control()
+    {
+
+        float keyHorizontal = Input.GetAxis("Horizontal");
+        float keyVertical = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.right * this.moving_speed * Time.smoothDeltaTime * keyHorizontal, Space.World);
+
+        transform.Translate(Vector3.forward * this.moving_speed * Time.smoothDeltaTime * keyVertical, Space.World);
+
+
+    }
+    // Start is called before the first frame update
+    void Ro()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Debug.Log(0);
+
+        h = h * this.rot_speed * Time.deltaTime;
+        v = v * this.rot_speed * Time.deltaTime;
+
+
+        transform.Rotate(Vector3.back * h);
+        transform.Rotate(Vector3.right * v);
+
+    }
+
+    void shot()
+    {
+        if(Input.GetKeyUp(KeyCode.Space))
+        Instantiate(obj, new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z),
+        Quaternion.identity);
+
+    }
+    void Start()
+    {
+        tra = GetComponent<Transform>();
+       // ani = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        player_Moving_Control();
+        Ro();
+        shot();
+    }
+}
